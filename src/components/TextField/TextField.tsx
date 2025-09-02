285246c6b025008ca932174db7af45771cd3306e
Svyatoslav Kozelchuk-Panulin <svyatoslavkozelchuk@gmail.com>
2025-09-02 16:15:17 +0300

    add solution

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -7,7 +7,7 @@ type Props = {
   label?: string;
   placeholder?: string;
   required?: boolean;
-  onChange?: (newValue: string) => void;
+  onChange: (newValue: string) => void;
 };
 
 function getRandomDigits() {
@@ -20,7 +20,7 @@ export const TextField: React.FC<Props> = ({
   label = name,
   placeholder = `Enter ${label}`,
   required = false,
-  onChange = () => {},
+  onChange,
 }) => {
   // generate a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
@@ -45,7 +45,7 @@ export const TextField: React.FC<Props> = ({
           })}
           placeholder={placeholder}
           value={value}
-          onChange={event => onChange(event.target.value)}
+          onChange={e => onChange(e.target.value)}
           onBlur={() => setTouched(true)}
         />
       </div>
